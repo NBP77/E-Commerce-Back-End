94a0a1dda1a0e7cc1aa58cbcb44de50a16c3f1a7
NBP77 <nbp77@outlook.com>
2021-04-12 15:32:55 -0400

    Added sequlize info to models and need to figure out how to reference the tags model id

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,23 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      // References the Product model's id.
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // References the Tag model's id.
+    }
+
   },
   {
     sequelize,
